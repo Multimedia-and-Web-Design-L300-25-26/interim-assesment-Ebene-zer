08e4fdb047a4fdb318e7cee8d0378b02e3127dc6
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-04-24 01:31:53 +0000

    feat(server): split app initialization from startup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import 'dotenv/config';
-import express from 'express';
+import app from './app.js';
 import connectDB from './config/db.js';
-
-const app = express();
-
-app.use(express.json());
 
 // Connect to the database
 connectDB();
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Server is up and running!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
