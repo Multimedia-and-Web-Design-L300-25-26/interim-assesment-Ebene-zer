2bfd45efe69d246e0039a41c82ad19c16c4fdf60
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-04-25 12:29:16 +0000

    feat(api): create register, login, & login

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
 	res.send('Server is up and running!');
 });
+
+app.use('/', authRoutes);
 
 export default app;
