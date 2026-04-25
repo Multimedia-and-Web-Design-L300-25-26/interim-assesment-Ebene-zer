59a5cb7d6673a2062e32e0e3997b89ad14172396
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-04-25 15:04:16 +0000

    update routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,9 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/logout', logoutUser);
-router.get('/profile', protect, getProfile);
+router.post('/auth/register', registerUser);
+router.post('/auth/login', loginUser);
+router.post('/auth/logout', logoutUser);
+router.get('/auth/profile', protect, getProfile);
 
 export default router;
